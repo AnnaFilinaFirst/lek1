4e678573a154fc19855d13aa32ec868da5a88b56
Filina.Anna <filina.hanna@yandex.ru>
2022-10-13 11:49:09 +0300

    Lucky

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Анна");
+{
+    Console.WriteLine("Ура, это же Аня!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
