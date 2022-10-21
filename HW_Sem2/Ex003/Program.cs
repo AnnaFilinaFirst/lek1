baa2c6cd2f5015a019c7bb60ca2dc4c4dbb09403
Filina.Anna <filina.hanna@yandex.ru>
2022-10-21 14:12:29 +0300

    Exercise003

---
+++ HW_Sem2/Ex003/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+while (number > 999)
+{
+   number = number / 10;
+}
+//Console.WriteLine($"получилось число {number}");
+if (number >= 100 && number <= 999)
+{
+    int result1 = number % 100;
+    int result2 = result1 % 10;
+    Console.WriteLine($"Третья цифра числа: {result2}");
+}
+else
+{
+    if(number >= 0 && number <=99)
+    {
+        Console.WriteLine("Число не имеет третьей цифры");
+    }
+    else
+    {
+        Console.WriteLine("Введите положительное число");
+    }
+}
