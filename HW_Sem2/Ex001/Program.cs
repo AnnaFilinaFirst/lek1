127f6b9e0af1e8b4389ae3b882a5414ca749a058
Filina.Anna <filina.hanna@yandex.ru>
2022-10-21 14:11:34 +0300

    Exercise001

---
+++ HW_Sem2/Ex001/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+    int result1 = number % 100;
+    int result2 = result1 / 10;
+    Console.WriteLine($"Вторая цифра трехзначного числа: {result2}");
+}
+else
+{
+    Console.WriteLine($"Некорректные данные");
+}
