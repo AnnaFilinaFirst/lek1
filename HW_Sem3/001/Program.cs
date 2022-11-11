4ba146b5fdfd07dc4c691b34b4fd6efc4187acaa
Filina.Anna <filina.hanna@yandex.ru>
2022-11-11 16:48:36 +0300

    HW_Sem3

--- HW_Sem3/001/Program.cs
+++ HW_Sem3/001/Program.cs
@@ -4,17 +4,19 @@
 12821 -> да
 23432 -> да*/
     Console.WriteLine("Введите пятизначное число.");
-    int number = int.Parse(Console.ReadLine());
-    
-    for (int i = 0; i < 100000; i++)
+    string number = Console.ReadLine();
+        //for (int i = 0; i < 100000; i++)
+        int length = number.Length;
+        if (length == 5)
+
     {
         if (number[0] == number[4] && number[1] == number[3])
         {
-        Console.WriteLine($"Число {number} является полиндромом");
+        Console.WriteLine($"Число {number} является палиндромом");
         } 
        else
         {
-            Console.WriteLine($"Число {number} НЕ является полиндромом");
+            Console.WriteLine($"Число {number} НЕ является палиндромом");
         }                
     }
 //int Num = 0;
